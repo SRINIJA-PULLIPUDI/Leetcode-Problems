be237af1671062bcff9f03a9de6d3784890b6868
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2025-10-11 00:47:23 +0530

    Time: 0 ms (100%), Space: 17 MB (98.01%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i=0,j=0,s=0;
+        int n = prices.size();
+        while(i<n and j<n){
+            if(prices[j]<=prices[i]){
+                i = j;
+                j++;
+            }else{
+                s+=(prices[j]-prices[i]);
+                i=j;
+                j++;   
+            }
+        }
+        return s;
+    }
+};
