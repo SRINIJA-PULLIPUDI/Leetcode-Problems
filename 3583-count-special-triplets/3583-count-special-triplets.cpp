74345e75555a6a5bf28565f482bbc22d08378f78
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2025-12-09 15:08:07 +0530

    Time: 1265 ms (11.31%), Space: 386.5 MB (58.03%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        map<long long,long long> left;
+        map<long long,long long> right;
+        long long res = 0;
+        long long mod = 1e9 + 7;
+        for(long long i=0;i<nums.size();i++){
+            right[nums[i]]++;
+        }
+        // for(auto i:right){
+        //     cout<<i.first<<" "<<i.second<<endl;
+        // }
+        left[nums[0]]++;
+        cout<<left[nums[0]]<<endl;
+        for(long long i=1;i<nums.size()-1;i++){
+            long long x = nums[i]*2;
+            if(nums[i]==0){
+                if(left[x]>=1 and (right[x]-left[x]-1>=1)){
+                    res += ((right[x]-left[x]-1)*left[x]);
+                    res %= mod;
+                }
+            }else{
+                if(left[x]>=1 and (right[x] - left[x])>=1){
+                    res += ((right[x]-left[x]) * left[x]);
+                    res %= mod;
+                }
+            }
+            left[nums[i]]++;
+        }
+        return res;
+    }
+};
