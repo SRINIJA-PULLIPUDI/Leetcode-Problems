83504150eba0a8b6a6cbdee5daf6fe269cac755d
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-02-08 05:18:44 +0530

    Time: 19 ms (73.82%), Space: 25.4 MB (98.09%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int b=0,res=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='b') b++;
+            else {
+                if(b>0){
+                    b--;
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
