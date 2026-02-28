2c0de5adda8226f0754023baee41ab398d7e18bb
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-02-28 21:35:26 +0530

    Time: 1182 ms (5.37%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,5 @@
+/* Write your PL/SQL query statement below */
+WITH cte AS 
+(SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary,RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rank FROM Employee e JOIN Department d
+ON e.departmentId = d.id)
+SELECT Department, Employee, Salary FROM cte WHERE rank = 1;
