aedde249df7749ed44cfd3c90f6bb79630d7fb67
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-03-15 00:00:35 +0530

    Time: 0 ms (100%), Space: 28.1 MB (81.57%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        vector<int> mp(101,0);
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0 and mp[nums[i]]==1) return nums[i];
+        }
+        return -1;
+    }
+};
