46bca7a0fa1bcbbe6b5dab94ecde52514cb754dd
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-05-30 11:33:22 +0530

    Time: 5 ms (83.6%), Space: 11.3 MB (53.53%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,16 +9,29 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head==nullptr) return nullptr;
-        map<ListNode*, int> mp;
-        ListNode * t = head;
-        while(t!=nullptr){
-            if(mp[t]){
-                return t;
+        if(head==nullptr) return head;
+        if((head->next) and head->next->next==nullptr) return nullptr;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        int flag=0;
+        while((fast!=nullptr and fast->next!=nullptr) or slow!=fast){
+            if(fast==nullptr or fast->next==nullptr or fast->next->next==nullptr) return nullptr;
+            if(flag==0){
+                slow = slow->next;
+                fast = fast->next->next;
             }else{
-                mp[t]++;
+                slow = slow->next;
+                fast = fast->next;
             }
-            t = t->next;
+            if(slow==fast){
+                if(slow == head) return head;
+                if(flag==0){
+                    slow = head;
+                    flag=1;
+                }else if(flag==1){
+                    return slow;
+                }
+            }
         }
         return nullptr;
     }
