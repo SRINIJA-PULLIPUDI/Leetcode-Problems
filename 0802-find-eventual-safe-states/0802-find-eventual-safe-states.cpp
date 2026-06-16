a093a0d5ec20edc5bc16c62998661f1c111ff5dc
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-06-16 14:20:27 +0530

    Time: 58 ms (18.2%), Space: 66.4 MB (19.92%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-bool dfs(int node, vector<int>& vis, vector<int>& path, vector<vector<int>>& graph,vector<int>& res){
-    for(int i=0;i<graph[node].size();i++){
-        if(vis[graph[node][i]]==0){
-            vis[graph[node][i]]=1;
-            path[graph[node][i]]=1;
-            bool r = dfs(graph[node][i],vis,path,graph,res);
-            if(r==false) return false;
-        }else{
-            if(path[graph[node][i]]==1) return false;
-        }
-        path[graph[node][i]]=0;
-    }
-    res.push_back(node);
-    return true;
-}
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> vis(graph.size());
-        vector<int> path(graph.size());
-        vector<int> res;
-        for(int i=0;i<vis.size();i++){
-            if(vis[i]==0){
-                vis[i]=1;
-                path[i]=1;
-                bool r = dfs(i,vis,path,graph,res);
-                if(r==true) path[i]=0;
+        vector<vector<int>> adj(graph.size());
+        vector<int> indegree(graph.size());
+        for(int i=0;i<graph.size();i++){
+            for(int j=0;j<graph[i].size();j++){
+                adj[graph[i][j]].push_back(i);
+            }
+        }for(int i=0;i<adj.size();i++){
+            for(int j=0;j<adj[i].size();j++){
+                indegree[adj[i][j]]++;
+            }
+        }queue<int> q;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i]==0) q.push(i);
+        }vector<int> res;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            res.push_back(node);
+            for(int i=0;i<adj[node].size();i++){
+                indegree[adj[node][i]]--;
+                if(indegree[adj[node][i]]==0) q.push(adj[node][i]);
             }
         }sort(res.begin(),res.end());
         return res;
