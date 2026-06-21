67fe7ab3c2c16cae15db04995e4c2059af66af06
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-06-21 12:54:43 +0530

    Time: 42 ms (19.75%), Space: 80.1 MB (89.61%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        int res=0;
+        for(int i=0;i<costs.size();i++){
+            if(costs[i]<=coins) {
+                coins-=costs[i];
+                res++;
+            }else break;
+        }return res;
+    }
+};
