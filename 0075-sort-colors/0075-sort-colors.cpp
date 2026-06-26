fd114de5cbfaa1d681852335aef60bcac9950e4f
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-06-27 02:33:19 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.34%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int i=0,k=nums.size()-1;
+        for(int j=0;j<nums.size();j++){
+            if(j>k) break;
+            if(nums[j]==0){
+                swap(nums[i],nums[j]);
+                i++;
+            }else if(nums[j]==2) {
+                swap(nums[j],nums[k]);
+                j--;
+                k--;
+            }
+        }
     }
 };
