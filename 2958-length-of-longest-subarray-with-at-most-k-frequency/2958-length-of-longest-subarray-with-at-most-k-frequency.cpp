7309229d907697cd231e1ea128e60fa95899de2a
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-08-12 21:26:26 +0530

    Time: 85 ms (14.33%), Space: 151.1 MB (12.98%) - LeetHub

---
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int i=0,j=0,maxi=1;
+        while(j<nums.size()){
+            mp[nums[j]]++;
+            while(mp[nums[j]]>k){
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0) mp.erase(nums[i]);
+                i++;
+            }
+            maxi = max(maxi, j-i+1);
+            j++;
+        }return maxi;
+    }
+};
